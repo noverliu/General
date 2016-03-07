f861e94e17c23447c4800f30712a020d65dc700d
noverliu <noverliu@sina.com>
2016-03-07 22:27:01 +0800

    1

--- SplitJson/Program.cs
+++ SplitJson/Program.cs
@@ -13,12 +13,17 @@ namespace SplitJson
     {
         static void Main(string[] args)
         {
+            var s = string.Format("{0}", "heelo", "world");
+            Console.Write(s);
+        }
+        static void Method01()
+        {
             string a = File.ReadAllText(Environment.CurrentDirectory + "\\Userinfo.json");
             UserCollection ul = JsonConvert.DeserializeObject<UserCollection>(a);
-            foreach(var u in ul.items)
+            foreach (var u in ul.items)
             {
                 string filename = u.id + ".json";
-                var fs=File.Create(filename);
+                var fs = File.Create(filename);
                 StreamWriter sr = new StreamWriter(fs);
                 sr.Write(JsonConvert.SerializeObject(u));
                 sr.Close();
